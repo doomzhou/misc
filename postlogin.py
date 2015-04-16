0bdf2554dfe9326ec59227ed970c4fc57dc21e8e
doomzhou <zzepaigh@gmail.com>
2015-04-16 17:53:35 +0800

    login

--- postlogin.py
+++ postlogin.py
@@ -9,23 +9,66 @@
 
 import requests
 import sys
-import bs4
+from bs4 import BeautifulSoup
+import logging
 
 
-data = {'name': 'zhoulifeng582', 'password': '5yixbDf6'}
-#data = {'name': 'zhoulifeng582', 'password': '5yixbDf6', 'rule_0': '1', 'rule_1': '1', 'rule_2': '1', 'rule_3': '0', 'auto_len': '23', 'min_len': '6', 'Submit': '%B5%C7+%C2%BC'}
-payload = {'bodyY': 670, 'bodyX': 1364}
-with requests.session() as c:
-    c.get('https://172.16.128.12/', params=payload, verify=False)
-    cookies = dict(c.cookies)
-    print(cookies)
-    c.post('https://172.16.128.12/login.php',cookies=cookies, data=data, params=payload, verify=False)
-    r = c.get('https://172.16.128.12/index.php', verify=False)
-    r.encoding='gb2312'
-    print(r.text)
-sys.exit(2)
-#s = requests.session()
+try:
+    import http.client as http_client
+except ImportError:
+    import httplib as http_client
+http_client.HTTPConnection.debuglevel = 1
+logging.basicConfig() 
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
 
-s.post(url='https://172.16.128.12/login.php?bodyY=273&bodyX=1364', cookies=cookies, params=payload, verify=False)
-r = s.get('https://172.16.128.12/module/cmd/realtime_report.txt?xy=1429101927', cookies=cookies,  verify=False)
+
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:35.0) Gecko/20100101 Firefox/35.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate',
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/x-www-form-urlencoded'
+        }
+url = 'https://172.16.128.12/login.php?bodyY=521&bodyX=1678'
+form_data = {
+        'name': 'zhoulifeng582',
+        'password': '5yixbDf6',
+        'rule_0': '1',
+        'rule_1': '1',
+        'rule_2': '1',
+        'rule_3': '0',
+        'auto_len': '23',
+        'min_len': '6',
+        'Submit': b'\xb5\xc7 \xc2\xbc'
+        }
+cookies = {'sdmenu_my_menu': '1000000', 'PLDSSID': 'a2laucg5dc571oldi0vp4e8440'}
+s = requests.session()
+s.verify = False
+s.headers = headers
+s.post(url, cookies=cookies, data=form_data)
+r = s.get('https://172.16.128.12/module/cmd/realtime_report.txt?xy=1429162541', cookies=cookies)
+print('doom')
 print(r.text)
+s.close()
+r = s.get('https://172.16.128.12/module/cmd/realtime_report.txt?xy=1429162541', cookies=cookies)
+print('doom')
+print(r.text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sys.exit(0)
+
