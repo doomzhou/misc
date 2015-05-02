df0867502a926aa2b9925a35339a61ec3f5c9a40
doomzhou <zzepaigh@gmail.com>
2015-05-02 13:49:00 +0800

    zhihu done

--- pytools/zhihu.py
+++ pytools/zhihu.py
@@ -8,12 +8,51 @@
 
 
 import sys
+import requests
+from bs4 import BeautifulSoup
+from mylog import logging
 
 
 PASS = ''
 
 
+def login():
+    url = 'http://www.zhihu.com'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:35.0)\
+                Gecko/20100101 Firefox/37.0',
+        'Accept': 'text/html,application/xhtml+xml,\
+                application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate',
+        'Connection': 'keep-alive',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Referer': 'http://www.zhihu.com/',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        }
+    s = requests.session()
+    s.headers = headers
+    r = s.get(url)
+    soup = BeautifulSoup(r.text)
+    dxsrf = soup.select('div form input:nth-of-type(6)')[0]['value']
+    data = {
+        '_xsrf': dxsrf,
+        'email': 'zzepaigh@gmail.com',
+        'password': PASS,
+        'rememberme': 'y'}
+    logurl = 'http://www.zhihu.com/login'
+    r1 = s.post(logurl, data=data)
+    r = s.get('http://www.zhihu.com/inbox')
+    print(r1.content)
+    print(r.url)
+    pass
+
+
 if __name__ == '__main__':
     if PASS == '':
-        sys.exit(2)
-    pass
+        logging.warning('Input your Zhihu password first')
+        sys.exit(0)
+    else:
+        login()
