cc66e79d879b30c360c0208ff6f91edebf094dd6
doomzhou <zzepaigh@gmail.com>
2015-05-02 11:28:41 +0800

    start zhihu

---
+++ pytools/zhihu.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File Name : zhihu.py
+'''Purpose : Intro sth                                 '''
+# Creation Date : 2015-02-05
+# Last Modified :
+# Release By : Doom.zhou
+
+
+import sys
+
+
+PASS = ''
+
+
+if __name__ == '__main__':
+    if PASS == '':
+        sys.exit(2)
+    pass
