88a5f9f4082aa704d87903b7dbc4a896721a01bf
doomzhou <zzepaigh@gmail.com>
2015-05-26 11:06:34 +0800

    nor

--- kivy-pratical/1.py
+++ kivy-pratical/1.py
@@ -11,5 +11,14 @@ class dApp(App):
         return PongGame()
 
 
+class PongBall(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
+
+
 if __name__ == '__main__':
     dApp().run()
