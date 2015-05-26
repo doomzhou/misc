0fb3893a1ba30a8aea8189a6b552319bde5103cb
doomzhou <zzepaigh@gmail.com>
2015-05-26 08:39:26 +0800

    nor

---
+++ kivy-pratical/first.py
@@ -0,0 +1,13 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+kivy.require('1.9.1')
+
+
+class MyApp(App):
+
+    def build(self):
+        return Label(text="HW")
+
+if __name__ == '__main__':
+    MyApp().run()
